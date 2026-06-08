b4f86316c24bfa9ffb945dc1f0000a652706173d
CascoArcilla <normon1230@gmail.com>
2026-06-08 17:46:32 -0600

    inyeccion de dependencias en app con OpenAPI

---
+++ Data/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Data.Persisten;
+using Data.Repositories;
+using Domain;
+using Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    // Inyeccion de depencias por medio de componenetes
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddData(this IServiceCollection services, string stringConnection)
+        {
+            // Hacer la conexion en a la DB
+            services.AddDbContext<AplicationDbContext>(options =>
+            options.UseSqlServer(stringConnection);
+
+            services.AddScoped<IRepository<PersonEntity, Guid>, PersonRepository>();
+            services.AddScoped<ICodeRepository<PersonEntity>, PersonRepository>();
+
+            return services;
+        }
+    }
+}
---
+++ WebApi/Program.cs
@@ -0,0 +1,69 @@
+using Aplication.UseCases.Persons;
+using Data;
+using Data.Repositories;
+using Domain;
+using Domain.Abstractions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+// Obtener la cadena de configuracoin a la DB especificada en el appsettings.json,
+// si no se encuentra se lanza una excepcion indicando que no hay configuracion a la DB
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Sin configuracion a la DB");
+
+// Los servicios de la capa de aplicación y de infraestructura se registran aquí
+// para que puedan ser inyectados en los controladores o en otros servicios.
+
+// Dependency Injection, these registrations are for the repositories and use cases
+builder.Services.AddData(connectionString); // Una forma de inyectar las depencias abajo por medio de componenete
+//builder.Services.AddScoped<IRepository<PersonEntity, Guid>, PersonRepository>();
+//builder.Services.AddScoped<ICodeRepository<PersonEntity>, PersonRepository>();
+
+// Register Use Cases
+builder.Services.AddScoped<CreatePersonUseCase>();
+builder.Services.AddScoped<GetPersonByIdUseCase>();
+builder.Services.AddScoped<GetPersonByCodeUseCase>();
+builder.Services.AddScoped<GetAllPersonsUseCase>();
+builder.Services.AddScoped<UpdatePersonUseCase>();
+builder.Services.AddScoped<DeletePersonUseCase>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
+
+/*
+// Esto es un ejemplo de metodos de extension
+// Una forma de agregar nuevos metodos a una clase ya compilada
+
+string name = "Bobi";
+
+// Este metodo no existe dentro de la clase string, pero lo podemos agregar con un metodo de extension
+
+Console.WriteLine(name.Run());
+
+// Este es el ejemplo de agregar un metodo de extension a la clase string,
+// el metodo se llama Run y devuelve un mensaje indicando que el string esta corriendo
+
+public static class StringExtensions
+{
+    // Se indica que el primer parametro es del tipo string y se le asigna el nombre str,
+    // esto indica que el metodo de extension se aplicara a la clase string
+
+    public static string Run(this string str)
+    {
+        return $"El string {str} esta corriendo";
+    }
+}
+*/
